8ba1423a9a5efdb47385001181e694537ec36694
David Eaton <DavidE@stocktrac.com>
2024-02-21 09:01:25 -0500

    Add TestController Fix launchsettings Remove wagger from startup

---
+++ Tymr.Api/Features/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tymr.Api.Features
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            // Constructor for the controller.
+            // In a more complex application, dependencies such as services would be injected here.
+        }
+
+        [HttpGet]
+        public IActionResult GetTestValue()
+        {
+            // Return a simple integer value with a 200 OK status; 42 is chosen as a simple example value.
+            return Ok(42);
+        }
+    }
+}
